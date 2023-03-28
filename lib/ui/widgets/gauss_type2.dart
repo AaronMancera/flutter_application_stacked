e89882e7dfa24a54fb8765e22d9919546436a94f
Aarón <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-03-28 13:00:58 +0000

    [ Anotacion ]
    Anotado los metodos de onViewModelReady y onDispose

--- lib/ui/widgets/gauss_type2.dart
+++ lib/ui/widgets/gauss_type2.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_stcaked/core/viewmodels/gauss_model.dart';
+import 'package:get_it/get_it.dart';
 import 'package:stacked/stacked.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class GaussType2 extends StackedView<GaussViewModel> {
   const GaussType2({super.key});
+//onviewmodleredit
+//Metodo de inciio de pantalla
+  @override
+  void onViewModelReady(GaussViewModel viewModel) {
+    // TODO: implement onViewModelReady
+    super.onViewModelReady(viewModel);
+  }
+//Metodo de salida de pantalla
+
+  @override
+  void onDispose(GaussViewModel viewModel) {
+    super.onDispose(viewModel);
+  }
 
   @override
   Widget builder(
